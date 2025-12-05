8976b78adfe912041d60dbfddc5b421dccaf23f0
RPJMICRO01 <rpjmicro@gmail.com>
2025-12-05 11:52:18 -0500

    variables-exercises

---
+++ 03-variables-holtel-challenge.py
@@ -0,0 +1,17 @@
+print("*** sistma de reserva de hoteles ***")
+
+cliente = "laura martinez"
+
+print("CLIENTE: ", cliente)
+
+estancia = "5D"
+
+print("DIAS DE ESTANCIA: ", estancia)
+
+tarifa = 1200.0
+
+print("tarifa diaria:", tarifa)
+
+con_vista_al_mar = True
+
+print("habitacion con vista al mar:  ", con_vista_al_mar)
---
+++ 03-variables-stock-shop.py
@@ -0,0 +1,11 @@
+print("*** tienda online ***")
+
+producto = "taza lego"
+precio = 7.50
+cantidad = 34
+disponible = True
+
+print("el producto es: " , producto)
+print("el precio es: " , precio)
+print("la cantidad es: " , cantidad)
+print("disponible: " , disponible)
